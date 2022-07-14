8e85b68bfa72705e4b1421497b817f9b4fb423ca
Jacob George <kooldrjay060@gmail.com>
2022-07-14 10:16:56 +0100

    run yarn dev

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,20 @@
 require('./bootstrap');
 
-import {createApp, h} from 'vue';
-import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
-import { ZiggyVue } from "ziggy";
-import { Ziggy } from '@/ziggy'
-
-InertiaProgress.init(); 
-
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    resolve: async (name) => {
-        return (await import(`./Pages/${name}`)).default;
-    },
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .component("Link", Link)
-            .component("Head", Head)
             .mixin({ methods: { route } })
             .mount(el);
     },
 });
+
+InertiaProgress.init({ color: '#4B5563' });
